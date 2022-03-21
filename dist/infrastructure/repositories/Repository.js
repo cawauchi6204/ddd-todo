0a71cb458da93eac5d399aa6d5cc795e03aa8ecc
cawauchi6204 <cawauchi6204@gmail.com>
2022-03-21 14:48:37 +0900

    I: distファイルを追跡から外す

--- dist/infrastructure/repositories/Repository.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.sequelize = void 0;
-const sequelize_1 = require("sequelize");
-require('dotenv').config();
-exports.sequelize = new sequelize_1.Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-    host: process.env.DB_HOST,
-    dialect: 'mysql',
-});
